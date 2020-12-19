915d57d3ef43880bcf1554d4f0b7365a623d2eee
yiyaowen <wenyiyao19@mails.ucas.ac.cn>
2020-12-19 12:58:39 +0800

    Fix bug [isCoreBoardFull] in CoreUtility.c

--- Src/Core.Main/CoreUtility.c
+++ Src/Core.Main/CoreUtility.c
@@ -4,7 +4,7 @@ bool isCoreBoardFull(CoreBoard board)
 {
     for (int i = 0; i < COR_BOARD_SIZE; ++i) {
         for (int j = 0; j < COR_BOARD_SIZE; ++j) {
-            if (board[i][j] != CPC_SIDE_EMPTY) return false;
+            if (board[i][j] == CPC_SIDE_EMPTY) return false;
         }
     }
     return true;
